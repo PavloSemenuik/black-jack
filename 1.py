9e4d07cc825a1e97475ac2d99d0892a3cbf14ea9
Semeniuk Pavlo <semeniuk_ak23@nuwm.edu.ua>
2025-02-21 13:45:26 +0200

    Implement Hand class for storing player's cards

--- 1.py
+++ 1.py
@@ -7,3 +7,28 @@ class Card:
 
     def __str__(self):
         return f"{self.rank} of {self.suit}"
+class Hand:
+    def __init__(self):
+        self.cards = []
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def get_value(self):
+        value = 0
+        aces = 0
+        for card in self.cards:
+            if card.rank in ['Jack', 'Queen', 'King']:
+                value += 10
+            elif card.rank == 'Ace':
+                value += 11
+                aces += 1
+            else:
+                value += int(card.rank)
+        while value > 21 and aces:
+            value -= 10
+            aces -= 1
+        return value
+
+    def __str__(self):
+        return ', '.join(str(card) for card in self.cards)
