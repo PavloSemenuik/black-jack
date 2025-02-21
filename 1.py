59b4a73732c9be74cfcaedd8a381f245bbf1fd1f
Semeniuk Pavlo <semeniuk_ak23@nuwm.edu.ua>
2025-02-21 13:44:53 +0200

    Add Card class for Black Jack game

--- 1.py
+++ 1.py
@@ -1,34 +1,9 @@
+# black_jack.py
 
-
-class Карти:
-    def __init__(self, ранг, масть):
-        self.ранг = ранг
-        self.масть = масть
-
-    def __str__(self):
-        return f"{self.ранг} {self.масть}"
-class Руки:
-    def __init__(self):
-        self.карти = []
-
-    def додати_карту(self, карта):
-        self.карти.append(карта)
-
-    def обчислити_значення(self):
-        значення = 0
-        тузів = 0
-        for карта in self.карти:
-            if карта.ранг in ['Валет', 'Дама', 'Король']:
-                значення += 10
-            elif карта.ранг == 'Туз':
-                значення += 11
-                тузів += 1
-            else:
-                значення += int(карта.ранг)
-        while значення > 21 and тузів:
-            значення -= 10
-            тузів -= 1
-        return значення
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
 
     def __str__(self):
-        return ', '.join(str(карта) for карта in self.карти)
+        return f"{self.rank} of {self.suit}"
